07889a9bf634a405a59e99ce7732c46800760208
loki-143 <chlokesh365@gmail.com>
2025-05-29 18:06:14 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent=React.createElement(
-    "div",
-     { id: "parent" },
-      React.createElement(
-        "div",
-        {id:"child"}, 
-        [React.createElement(
-            "h1",
-            null,
-            "Hello World"),
-        React.createElement(
-            "p",
-            null,
-            "This is a paragraph"
-        )
-      ]));
-console.log(parent);
+const header=React.createElement(
+    "h1",
+    {},
+    "Hello, World!"
+)
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(
+    header
+);
